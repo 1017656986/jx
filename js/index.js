fb317ed3e9ffd13df850285bb71cb906a1c5509b
你说。 <1017656986@qq.com>
2021-10-20 22:05:51 +0800

    Revert "小蒋的网站"
    This reverts commit 24c533b19c11bede8ef0c9dcb3d771c052977f3f.

--- js/index.js
+++
@@ -1,9 +0,0 @@
-function getinput() {
-    var res = document.getElementById('input').value;
-    if (res == "") {
-        alert('请您输入要解析的链接或片名');
-    } else {
-        var url = document.getElementById("jx");
-        url.href = "https://z1.m1907.cn/?jx=" + res;
-    }
-}
