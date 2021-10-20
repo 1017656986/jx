4ec51dec401ebe03d3092abb2d4d9c9689de7eb5
你说。 <1017656986@qq.com>
2021-10-20 22:02:39 +0800

    Revert "jx"
    This reverts commit a83051793d32ef5b9cee39ea88ff055e9ebb41a1.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,9 @@
 function getinput() {
-    // 输入框
-    var input = document.getElementById('input').value;
-    if (input === "") {
+    var res = document.getElementById('input').value;
+    if (res == "") {
         alert('请您输入要解析的链接或片名');
     } else {
-        // // 解析按钮
-        var Resolve = document.getElementById("Resolve");
-        var Resolve_source = document.getElementById("Resolve_source");
-        var Parsing_url = Resolve_source.options[Resolve_source.selectedIndex].value;
-        Resolve.href = ""+Parsing_url + input
+        var url = document.getElementById("jx");
+        url.href = "https://z1.m1907.cn/?jx=" + res;
     }
 }
