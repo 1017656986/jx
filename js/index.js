a83051793d32ef5b9cee39ea88ff055e9ebb41a1
你说。 <1017656986@qq.com>
2021-10-20 21:49:48 +0800

    jx
    jx

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
 function getinput() {
-    var res = document.getElementById('input').value;
-    if (res == "") {
+    // 输入框
+    var input = document.getElementById('input').value;
+    if (input === "") {
         alert('请您输入要解析的链接或片名');
     } else {
-        var url = document.getElementById("jx");
-        url.href = "https://z1.m1907.cn/?jx=" + res;
+        // // 解析按钮
+        var Resolve = document.getElementById("Resolve");
+        var Resolve_source = document.getElementById("Resolve_source");
+        var Parsing_url = Resolve_source.options[Resolve_source.selectedIndex].value;
+        Resolve.href = ""+Parsing_url + input
     }
 }
